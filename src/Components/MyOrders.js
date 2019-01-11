d6c8e664ea50792b4ead65473376817e4535c305
reactexcel <arun.excel2011@gmail.com>
2019-01-11 18:34:53 +0530

    added orders

--- src/Components/MyOrders.js
+++ src/Components/MyOrders.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Collapsible from "react-collapsible";
 class MyOrders extends Component {
   render() {
     return (
@@ -10,77 +10,99 @@ class MyOrders extends Component {
               <div className="col-xs-12 col-sm-12 col-md-12">
                 <div className="my-orders-section">
                   <h3 align="center"> My Orders</h3>
-                  <button className="collapsible">
-                    <ul className="tab_order">
-                      <li className="orders1">
-                        <span className="small_title"> Order No. </span>
-                        <span>W1HQD3CC</span>
-                      </li>
-                      <li className="orders2">
-                        <span className="small_title">Status</span>
-                        <span className="link"> Outstanding medical visit </span>
-                      </li>
-                      <li className="orders3">
-                        <span className="small_title">Tracking Number</span>
-                        <span>-</span>
-                      </li>
-                      <li className="orders4">
-                        <i className="fa fa-angle-right" />
-                        <i className="fa fa-angle-up" />
-                      </li>
-                    </ul>
-                  </button>
+                  <Collapsible
+                    trigger={
+                      <button type="button">
+                        <ul className="tab_order">
+                          <li className="orders1">
+                            <span className="small_title"> Order No. </span>
+                            <span>W1HQD3CC</span>
+                          </li>
+                          <li className="orders2">
+                            <span className="small_title">Status</span>
+                            <span className="link">
+                              {" "}
+                              Outstanding medical visit{" "}
+                            </span>
+                          </li>
+                          <li className="orders3">
+                            <span className="small_title">Tracking Number</span>
+                            <span>-</span>
+                          </li>
+                          <li className="orders4">
+                            <i className="fa fa-angle-right" />
+                            {/* <i className="fa fa-angle-up" /> */}
+                          </li>
+                        </ul>
+                      </button>
+                    }
+                  >
+                    <div className="content">
+                      <ul className="open_tab_order">
+                        <li className="add1">
+                          <span className="small_title">Amount</span> $35.00{" "}
+                          <br />
+                          <br />
+                          <span>
+                            <span className="small_title">
+                              Shipping Address
+                            </span>{" "}
+                            1031 n 3rd st., 101 <br /> philadelphia, PA <br />{" "}
+                            19123
+                            <br /> USA
+                          </span>
+                        </li>
+                        <li className="add2"> Sildenafil x1 </li>
+                      </ul>
+                    </div>
+                  </Collapsible>
 
-                  <div className="content">
-                    <ul className="open_tab_order">
-                      <li className="add1">
-                        <span className="small_title">Amount</span> $35.00 <br />
-                        <br />
-                        <span>
-                          <span className="small_title">Shipping Address</span> 1031
-                          n 3rd st., 101 <br /> philadelphia, PA <br /> 19123
-                          <br /> USA
-                        </span>
-                      </li>
-                      <li className="add2"> Sildenafil x1 </li>
-                    </ul>
-                  </div>
-
-                  <button className="collapsible">
-                    <ul className="tab_order">
-                      <li className="orders1">
-                        <span className="small_title"> Order No. </span>
-                        <span>8PgeWmxo</span>
-                      </li>
-                      <li className="orders2">
-                        <span className="small_title">Status</span>
-                        <span className="link"> Outstanding medical visit </span>
-                      </li>
-                      <li className="orders3">
-                        <span className="small_title">Tracking Number</span>
-                        <span>-</span>
-                      </li>
-                      <li className="orders4">
-                        <i className="fa fa-angle-right" />
-                        <i className="fa fa-angle-up" />
-                      </li>
-                    </ul>
-                  </button>
-
-                  <div className="content">
-                    <ul className="open_tab_order">
-                      <li className="add1">
-                        <span className="small_title">Amount</span> $25.00 <br />
-                        <br />
-                        <span>
-                          <span className="small_title">Shipping Address</span> 1031
-                          n 3rd st., 101 <br /> philadelphia, PA <br /> 19123
-                          <br /> USA
-                        </span>
-                      </li>
-                      <li className="add2"> Sildenafil x1 </li>
-                    </ul>
-                  </div>
+                  <Collapsible
+                    trigger={
+                      <button className="collapsible">
+                        <ul className="tab_order">
+                          <li className="orders1">
+                            <span className="small_title"> Order No. </span>
+                            <span>8PgeWmxo</span>
+                          </li>
+                          <li className="orders2">
+                            <span className="small_title">Status</span>
+                            <span className="link">
+                              {" "}
+                              Outstanding medical visit{" "}
+                            </span>
+                          </li>
+                          <li className="orders3">
+                            <span className="small_title">Tracking Number</span>
+                            <span>-</span>
+                          </li>
+                          <li className="orders4">
+                            <i className="fa fa-angle-right" />
+                            <i className="fa fa-angle-up" />
+                          </li>
+                        </ul>
+                      </button>
+                    }
+                  >
+                    <div className="content">
+                      <ul className="open_tab_order">
+                        <li className="add1">
+                          <span className="small_title">Amount</span> $25.00{" "}
+                          <br />
+                          <br />
+                          <span>
+                            <span className="small_title">
+                              Shipping Address
+                            </span>{" "}
+                            1031 n 3rd st., 101 <br /> philadelphia, PA <br />{" "}
+                            19123
+                            <br /> USA
+                          </span>
+                        </li>
+                        <li className="add2"> Sildenafil x1 </li>
+                      </ul>
+                    </div>
+                  </Collapsible>
                 </div>
               </div>
             </div>
--- src/Containers/OrdersContainer.js
+++ src/Containers/OrdersContainer.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import Header from '../Components/Generic/Header';
-import Footer from '../Components/Generic/Footer';
-import Orders from '../Components/Orders';
+import React, { Component } from "react";
+import Header from "../Components/Generic/Header";
+import Footer from "../Components/Generic/Footer";
+import Orders from "../Components/Orders";
+import MyOrders from "../Components/MyOrders";
 
 class OrdersContainer extends Component {
   render() {
     return (
       <div>
-      	<Header/>
-        	<Orders/>
-        <Footer/>
+        <Header />
+        {/* <Orders /> */}
+        <MyOrders />
+        <Footer />
       </div>
     );
   }
