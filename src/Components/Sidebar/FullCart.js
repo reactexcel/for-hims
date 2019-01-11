2e4900a2f02517e3d7b4686634a57396664e8251
reactexcel <arun.excel2011@gmail.com>
2019-01-11 20:02:56 +0530

    login added when not login for auth

--- src/Components/Sidebar/FullCart.js
+++ src/Components/Sidebar/FullCart.js
@@ -4,31 +4,49 @@ import Shipping from "./Shipping";
 import ConfirmOrder from "./ConfirmOrder";
 import Payment from "./Payment";
 import VerifyAddress from "./VerifyAddress";
+import Login from "./Login";
+import { connect } from "react-redux";
 
-export default class FullCart extends Component {
+class FullCart extends Component {
   state = {
     next: 1
   };
   renderNext = () => {
-    this.setState(prevState => ({ next: prevState.next + 1 }));
+    const auth = JSON.parse(localStorage.getItem("auth"));
+    if (!this.props.login.auth && !auth) {
+      this.setState({ next: 10 });
+    } else if (this.state.next < 6) {
+      this.setState(prevState => ({ next: prevState.next + 1 }));
+    }
   };
   _renderItem = () => {
     switch (this.state.next) {
       case 1:
-        return <Cart closeSidebar={this.props.closeSidebar} renderNext={this.renderNext}/>;
+        return (
+          <Cart
+            closeSidebar={this.props.closeSidebar}
+            renderNext={this.renderNext}
+          />
+        );
       case 2:
-        return <Shipping renderNext={this.renderNext}/>;
+        return <Shipping renderNext={this.renderNext} />;
       case 3:
-        return <VerifyAddress renderNext={this.renderNext}/>;
+        return <VerifyAddress renderNext={this.renderNext} />;
       case 4:
-        return <Payment renderNext={this.renderNext}/>;
+        return <Payment renderNext={this.renderNext} />;
       case 5:
-        return <ConfirmOrder/>;
+        return <ConfirmOrder />;
+      case 10:
+        return <Login />;
       default:
         return;
     }
   };
   render() {
+    console.log(this.state.next);
     return <>{this._renderItem()}</>;
   }
 }
+const mapStateToProps = ({ login }) => ({ login });
+
+export default connect(mapStateToProps)(FullCart);
