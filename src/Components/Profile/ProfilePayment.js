a0108f61dd692ff683b08a73acb0daa50c9ece03
reactexcel <arun.excel2011@gmail.com>
2019-01-14 18:27:10 +0530

    added stripe with validation

--- src/Components/Profile/ProfilePayment.js
+++ src/Components/Profile/ProfilePayment.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { CardElement, injectStripe } from "react-stripe-elements";
+import ErrorText from "../Generic/ErrorText";
 
-function ProfilePayment() {
-  return (
-    <div className="profile_module">
-      <h3>Payment Methods</h3>
-      <form>
-        {/* <input
+class ProfilePayment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {}
+    };
+  }
+  submit = async e => {
+    console.log(e, "evevwefv");
+    let { token } = await this.props.stripe.createToken({ name: "Name" });
+    console.log(token);
+  };
+  validateCard = e => {
+    const errors = {};
+    if (e.elementType === "card" && e.error !== undefined) {
+      errors.message = e.error.message;
+      this.setState({ errors });
+    } else if (e.error === undefined) {
+      this.setState({ errors: {} });
+    }
+  };
+  render() {
+    const {
+      errors
+    } = this.state;
+    return (
+      <div className="profile_module">
+        <h3>Payment Methods</h3>
+        <form>
+          {/* <input
           type="text"
           className="card"
           name="firstName"
@@ -22,12 +47,16 @@ function ProfilePayment() {
           autoComplete="true"
           placeholder="MM / YY / CVC"
          /> */}
-        <CardElement />
-        <button type="button">Add New Payment Method</button>
-      </form>
-      <Link to="#">Cancel</Link>
-    </div>
-  );
+          <CardElement onChange={this.validateCard} />
+          { errors.message && <ErrorText text={errors.message} />}
+          <button type="button" onClick={this.submit}>
+            Add New Payment Method
+          </button>
+        </form>
+        <Link to="#">Cancel</Link>
+      </div>
+    );
+  }
 }
 
 export default injectStripe(ProfilePayment);
