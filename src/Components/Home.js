80332fdbbaa6ec016e909730e00b0097495c59e5
reactexcel <rkumar@netchexonline.com>
2019-07-17 16:42:27 +0530

    5 section is done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,31 +7,41 @@ import hims_pdp_sildenafil_img3 from "../assets/images/hims_pdp_sildenafil_img3.
 import hims_pdp_sildenafil_img4 from "../assets/images/hims_pdp_sildenafil_img4.jpg";
 import hims_pdp_sildenafil_img5 from "../assets/images/hims_pdp_sildenafil_img5.jpg";
 import hims_pdp_sildenafil_img6 from "../assets/images/hims_pdp_sildenafil_img6.jpg";
-import home_bg from "../assets/images/home-bg.png"
-import endo1 from "../assets/images/FDACLEARED-white.png"
-import endo2 from "../assets/images/hi-res-logo-registered.png"
-import endo3 from "../assets/images/umassmedical-vitiligo-logo-white.png"
-import uv_light from "../assets/images/UV-light.png"
-import before_1 from "../assets/images/before-1.png"
-import before_2 from "../assets/images/before-2.png"
-import before_3 from "../assets/images/before-3.png"
-import before_4 from "../assets/images/before-4.png"
-import video_1 from "../assets/images/video-1.png"
-import video_2 from "../assets/images/the-science.png"
-
+import home_bg from "../assets/images/home-bg.png";
+import endo1 from "../assets/images/FDACLEARED-white.png";
+import endo2 from "../assets/images/hi-res-logo-registered.png";
+import endo3 from "../assets/images/umassmedical-vitiligo-logo-white.png";
+import uv_light from "../assets/images/UV-light.png";
+import before_1 from "../assets/images/before-1.png";
+import before_2 from "../assets/images/before-2.png";
+import before_3 from "../assets/images/before-3.png";
+import before_4 from "../assets/images/before-4.png";
+import video_1 from "../assets/images/video-1.png";
+import video_2 from "../assets/images/the-science.png";
+import { Accordion, Button, Card } from "react-bootstrap";
+import cloneDeep from "lodash/cloneDeep";
 /**UI Component for Home page */
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { showInfo: false };
+    this.state = { showInfo: false, accordion: {} };
   }
-
+  handleClick = e => {
+    const clonedState = cloneDeep(this.state);
+    for (let i in clonedState.accordion) {
+      clonedState.accordion[i] = false;
+    }
+    clonedState.accordion[e] = !this.state.accordion[e];    
+    this.setState({
+      ...clonedState
+    });
+  };
   /**Toggles the information block*/
   toggleInfo = () =>
     this.setState(prevState => ({ showInfo: !prevState.showInfo }));
   render() {
     const { showInfo } = this.state;
-    const { onAddProduct, role } = this.props;
+    const { onAddProduct, role } = this.props;        
     return (
       <>
         <div className="header_slider">
@@ -39,46 +49,69 @@ class Home extends Component {
             <div className="row">
               {/* <div className="col-xs-12 col-sm-12 col-md-2"> </div> */}
               <div className="col-xs-12 col-sm-12 col-md-8 text-center first-home-section ">
-              <img src={uv_light} className="uv-light"/>
-              <div className="content">
-              <h1>TREAT</h1>
-              <h1>VITILIGO</h1>
-              <p>NATURALLY. EFFECTIVE. AT HOME.</p>
-              <div className="description">
-              <p>Clinically Proven, Trusted Worldwide. Phototherapy is the only FDA</p>
-                <p> cleared treatment for Vitiligo. The Noleuderm System couples a portable,</p>
-                <p> handheld phototherapy lamp with a specially formulated lotion in order to </p>
-                <p> ensure an optimal healing process and results.         </p>
-              </div>
-              <div className="endorsed">
-              <div>Endorsed By</div>
-              <div className="endorsed-list">
-                <img src={endo1}/>
-                <img src={endo2}/>
-                <img src={endo3}/>
-
-              </div>
-              </div>
-              </div>
+                <img src={uv_light} className="uv-light" />
+                <div className="content">
+                  <h1>TREAT</h1>
+                  <h1>VITILIGO</h1>
+                  <p>NATURALLY. EFFECTIVE. AT HOME.</p>
+                  <div className="description">
+                    <p>
+                      Clinically Proven, Trusted Worldwide. Phototherapy is the
+                      only FDA
+                    </p>
+                    <p>
+                      {" "}
+                      cleared treatment for Vitiligo. The Noleuderm System
+                      couples a portable,
+                    </p>
+                    <p>
+                      {" "}
+                      handheld phototherapy lamp with a specially formulated
+                      lotion in order to{" "}
+                    </p>
+                    <p> ensure an optimal healing process and results. </p>
+                  </div>
+                  <div className="endorsed">
+                    <div>Endorsed By</div>
+                    <div className="endorsed-list">
+                      <img src={endo1} />
+                      <img src={endo2} />
+                      <img src={endo3} />
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="col-xs-12 col-sm-12 col-md-3">
                 <div className="baseballcard__container">
                   <div className="baseball-card__inset">
-                  <div className="pricing">
-                    <h1 className="baseball-card__header">
-                    $49
-                    </h1>
-                   <div>
-                     <div style={{fontSize:"1.3em",marginTop:"10px",fontWeight:"600"}}>MONTH</div>
-                     <div style={{fontSize:"12px",color:"#414142",fontWeight:"lighter"}}>cancel anytime</div>
-                   </div>
+                    <div className="pricing">
+                      <h1 className="baseball-card__header">$49</h1>
+                      <div>
+                        <div
+                          style={{
+                            fontSize: "1.3em",
+                            marginTop: "10px",
+                            fontWeight: "600"
+                          }}
+                        >
+                          MONTH
+                        </div>
+                        <div
+                          style={{
+                            fontSize: "12px",
+                            color: "#414142",
+                            fontWeight: "lighter"
+                          }}
+                        >
+                          cancel anytime
+                        </div>
+                      </div>
                     </div>
                     {/* {(role === ROLES.CUSTOMER  || role === ROLES.DOCTOR)  && ( */}
-                      <Link to="#" className="btn1" onClick={onAddProduct}>
-                        START <i class="fa fa-arrow-right"></i>
-                      </Link>
+                    <Link to="#" className="btn1" onClick={onAddProduct}>
+                      START <i class="fa fa-arrow-right" />
+                    </Link>
                     {/* )} */}
-                
                   </div>
                 </div>
               </div>
@@ -86,22 +119,29 @@ class Home extends Component {
           </div>
         </div>
         <div className="clearfix" />
-      <div className="before-after">
-      <div className="main-heading">Before & After</div>
-      <p>The Noleuderm System uses the same phototherapy bulbs found in the leading</p>
-      <p className="before-after-content"> dermatology offices. Achieve in-office results in the comfort of your own home.</p>
-      <div class="grid-container effect-month">
-      <img src={before_1} alt="" className="grid-item"/>
-      <img src={before_2} alt="" className="grid-item"/>
-      <img src={before_3} alt="" className="grid-item"/>
-      <img src={before_4} alt="" className="grid-item"/>
+        <div className="before-after">
+          <div className="main-heading">Before & After</div>
+          <p>
+            The Noleuderm System uses the same phototherapy bulbs found in the
+            leading
+          </p>
+          <p className="before-after-content">
+            {" "}
+            dermatology offices. Achieve in-office results in the comfort of
+            your own home.
+          </p>
+          <div class="grid-container effect-month">
+            <img src={before_1} alt="" className="grid-item" />
+            <img src={before_2} alt="" className="grid-item" />
+            <img src={before_3} alt="" className="grid-item" />
+            <img src={before_4} alt="" className="grid-item" />
 
-         <div className="grid-item">2 months</div>
-         <div className="grid-item">4 months</div>
-         <div className="grid-item">6 months</div>
-         <div className="grid-item">8 months</div>
-      </div>
-      </div>
+            <div className="grid-item">2 months</div>
+            <div className="grid-item">4 months</div>
+            <div className="grid-item">6 months</div>
+            <div className="grid-item">8 months</div>
+          </div>
+        </div>
         <div className="clearfix" />
 
         <div className="doctor_recommended_section">
@@ -111,10 +151,12 @@ class Home extends Component {
                 <div className="recommended-callout-wrapper">
                   <div className="callout-inset">
                     <h1 className="callout-title"> DOCTOR RECOMMENDED </h1>
-                    <p>UV Phototherapy is the most widely recommended
-                     solution for Vitiligo by doctors worldwide due to its 
-                     clinical validation, safety, and effectiveness.</p>
-                   <button className="play">PLAY VIDEO</button>
+                    <p>
+                      UV Phototherapy is the most widely recommended solution
+                      for Vitiligo by doctors worldwide due to its clinical
+                      validation, safety, and effectiveness.
+                    </p>
+                    <button className="play">PLAY VIDEO</button>
                   </div>
                 </div>
               </div>
@@ -134,7 +176,7 @@ class Home extends Component {
         <div className="science_section">
           <div className="container-fluid">
             <div className="row">
-            <div className="col-xs-12 col-sm-12 col-md-4">
+              <div className="col-xs-12 col-sm-12 col-md-4">
                 {" "}
                 <img
                   src={video_2}
@@ -147,50 +189,105 @@ class Home extends Component {
                   <div className="callout-inset">
                     <h1 className="callout-title"> THE SCIENCE </h1>
                     <p>
-                    The Noleuderm System relies on continued and concentrated exposure of
-                     Vitiliginous areas to narrowband UV Phototherapy via its portable
-                      handheld lamp coupled with its specially formulated lotion to promote
-                     effective healing and results.
+                      The Noleuderm System relies on continued and concentrated
+                      exposure of Vitiliginous areas to narrowband UV
+                      Phototherapy via its portable handheld lamp coupled with
+                      its specially formulated lotion to promote effective
+                      healing and results.
                     </p>
-                   <button className="play">PLAY VIDEO</button>
+                    <button className="play">PLAY VIDEO</button>
                   </div>
                 </div>
               </div>
-
-            
             </div>
           </div>
         </div>
         {/* <div className="full_bg" /> */}
         <div className="clearfix" />
 
-        <div className="stay_hard_section">
+        <div className="system_section">
           <div className="container-fluid">
             <div className="row">
               <div className="col-xs-12 col-sm-12 col-md-6">
                 <div className="callout-wrapper">
                   <div className="callout-inset">
-                    <h1 className="callout-title"> It's science </h1>
-                    <h3>
-                      Viagra has been prescribed by doctors <br /> across the US
-                      for the last 20 years. It <br /> works, it’s effective,
-                      and we’re now <br /> connecting guys directly to what they{" "}
-                      <br /> need.
-                    </h3>
-                    <Link to="#" className="learn">
-                      learn: all about ED treatments
-                    </Link>
+                    <h1 className="callout-title"> THE SYSTEM </h1>
+                    <p>
+                      Unlike other Vitiligo treatments found online, the
+                      Noleuderm System is the only one to offer clinical
+                      validation and doctor endorsement. The reason? UV
+                      Phototherapy works. But this effectiveness takes time and
+                      frequency. To make this affordable, Noleuderm offers its
+                      products on a subscription basis.
+                    </p>
                   </div>
                 </div>
               </div>
 
               <div className="col-xs-12 col-sm-12 col-md-6">
-                {" "}
-                <img
-                  src={hims_pdp_sildenafil_img3}
-                  className="product-img2"
-                  alt="Sildenafil"
-                />{" "}
+                <Accordion className="system-accordion">
+                  <>
+                    <Accordion.Toggle
+                      as={Button}
+                      variant="link"
+                      eventKey="0"
+                      onClick={() => this.handleClick(0)}
+                    >
+                      <>
+                       <span className={!this.state.accordion[0]?"open-left":"open-right"}><span>1</span></span> START SUBSCRIPTION <i class="fa fa-angle-down" />
+                      </>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="0">
+                      <>asasd</>
+                    </Accordion.Collapse>
+                  </>
+                  <>
+                    <Accordion.Toggle
+                      as={Button}
+                      variant="link"
+                      eventKey="1"
+                      onClick={() => this.handleClick(1)}
+                    >
+                      <span className={!this.state.accordion[1]?"open-left":"open-right"}><span>2</span></span>VIRTUAL APPOINTMENT <i class="fa fa-angle-down" />
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="1">
+                      <>asdasdasd</>
+                    </Accordion.Collapse>
+                  </>
+                  <>
+                    <Accordion.Toggle
+                      as={Button}
+                      variant="link"
+                      eventKey="2"
+                      onClick={() => this.handleClick(2)}
+                    >
+                      <>
+                      <span className={!this.state.accordion[2]?"open-left":"open-right"}><span>3</span></span>  DOCTOR APPROVAL, DELIVERY <i class="fa fa-angle-down" />
+                      </>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="2">
+                      <>asasd</>
+                    </Accordion.Collapse>
+                  </>
+                  <>
+                    <Accordion.Toggle
+                      as={Button}
+                      variant="link"
+                      eventKey="3"
+                      onClick={() => this.handleClick(3)}
+                    >
+                    <span className={!this.state.accordion[3]?"open-left":"open-right"}><span>4</span></span>  CONTINUED USE & CHECK-UPS <i class="fa fa-angle-down" />
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="3">
+                      <>
+                        Apply the Noleuderm solution and expose the affected
+                        areas of your skin to the UV light for 10 - 15 minutes a
+                        day. Continue this process until the affected areas of
+                        your skin regrow pigment as desired.
+                      </>
+                    </Accordion.Collapse>
+                  </>
+                </Accordion>
               </div>
             </div>
           </div>
